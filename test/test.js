9acbc95234a480d451d0ae0d3dcf26762c69fc41
Micooz <micooz@hotmail.com>
2015-10-05 12:17:22 +0800

    Changed: simplify tests.

--- test/test.js
+++ test/test.js
@@ -15,106 +15,114 @@ const {
   Give
   } = Commands;
 
-const target_selector = new TargetSelector('p', {
-  coordinate: [100, undefined, 300]
-});
-
-describe('Target Selector', function () {
-
-  describe('#new(variable)', function () {
-    let target = new TargetSelector('p');
-    it('should return @p', function () {
-      assert.equal(target.toString(), '@p');
-    });
-  });
-
-  describe('#new(variable, args)', function () {
-    let target = new TargetSelector('p', {
-      coordinate: [1, undefined, 3]
-    });
-    it('should return @p[x=1,z=3]', function () {
-      assert.equal(target.toString(), '@p[x=1,z=3]');
-    });
-
-    it('should throw an error', function () {
-      (function () {
+const testCases = [
+  {
+    title: 'Target Selector',
+    methods: [{
+      title: '#new(variable)',
+      real: () => {
+        const target_selector = new TargetSelector('p', {
+          coordinate: [100, undefined, 300]
+        });
+        return target_selector.toString();
+      },
+      actual: '@p[x=100,z=300]'
+    }, {
+      title: '#new(variable, args)',
+      err: () => {
         new TargetSelector('p', {
           unknown: null
         });
-      }).should.throw();
-    });
-  });
-
-});
-
-describe('Say Command', function () {
-
-  describe('#new(msg)', function () {
-    let say = new Say('hi there');
-    it('should return /say hi there', function () {
-      assert.equal(say.toString(), '/say hi there');
-    });
-
-    it('should throw an error', function () {
-      (function () {
+      }
+    }]
+  }, {
+    title: 'Say Command',
+    methods: [{
+      title: '#new(msg)',
+      real: () => {
+        const say = new Say('hi there');
+        return say.toString();
+      },
+      actual: '/say hi there'
+    }, {
+      title: '#new(null)',
+      err: () => {
         new Say(null);
-      }).should.throw();
-    });
+      }
+    }]
+  }, {
+    title: 'Tell Command',
+    methods: [{
+      title: '#new(player, msg)',
+      real: () => {
+        const tell = new Tell(new Player('Mike'), 'hi Mike');
+        return tell.toString();
+      },
+      actual: '/tell Mike hi Mike'
+    }, {
+      title: '#new(player, msg)',
+      real: () => {
+        const tell = new Tell(
+          new Player(new TargetSelector('a', {radius: [10, 40]})),
+          'hi friends'
+        );
+        return tell.toString();
+      },
+      actual: '/tell @a[rm=10,r=40] hi friends'
+    }]
+  }, {
+    title: 'Spawnpoint Command',
+    methods: [{
+      title: '#new(player, coordinate)',
+      real: () => {
+        const spawn = new Spawnpoint(
+          new Player(new TargetSelector('p', {
+            coordinate: [100, undefined, 300]
+          })),
+          new Coordinate(10, 20, 30)
+        );
+        return spawn.toString();
+      },
+      actual: '/spawnpoint @p[x=100,z=300] 10 20 30'
+    }]
+  }, {
+    title: 'Give Command',
+    methods: [{
+      title: '#new(player, item, amount, data, dataTag)',
+      real: () => {
+        const give = new Give(
+          new Player('Mike'),
+          new Item('apple'),
+          64,
+          1,
+          {display: {Lore: ["Apple here"]}}
+        );
+        return give.toString();
+      },
+      actual: '/give Mike minecraft:apple 64 1 {display:{Lore:["Apple here"]}}'
+    }]
+  }
+];
+
+for (let i = 0, len = testCases.length; i < len; ++i) {
+  const test = testCases[i];
+  const methods = test.methods;
+  describe(test.title, () => {
+    for (let k = 0, len = methods.length; k < len; ++k) {
+      const method = methods[k];
+      describe(method.title, () => {
+        if (method.hasOwnProperty('actual') && method.hasOwnProperty('real')) {
+          it(`should return ${method.actual}`, () => {
+            assert.equal(method.real(), method.actual);
+          })
+        } else if (method.hasOwnProperty('err')) {
+          it('should throw an error', () => {
+            method.err.should.throw();
+          });
+        } else {
+          throw new Error('unresolved form of test method');
+        }
+      });
+    } // for
   });
-
-});
-
-describe('Tell Command', function () {
-
-  describe('#new(player, msg)', function () {
-    let player = new Player('Jake');
-    let tell = new Tell(player, 'hi Jake');
-    it('should return /tell Jake hi Jake', function () {
-      assert.equal(tell.toString(), '/tell Jake hi Jake');
-    });
-
-    let ts = new TargetSelector('p', {
-      coordinate: [10, 20, 30],
-      radius: [10, 40]
-    });
-    let player2 = new Player(ts);
-    let tell2 = new Tell(player2, 'hi Mike');
-    it('should return /tell @p[x=10,y=20,z=30,rm=10,r=40] hi Mike', function () {
-      assert.equal(tell2.toString(), '/tell @p[x=10,y=20,z=30,rm=10,r=40] hi Mike');
-    });
-  });
-
-});
-
-describe('Spawnpoint Command', function () {
-
-  describe('#new(player, coordinate)', function () {
-    let spawn = new Spawnpoint(
-      new Player(target_selector),
-      new Coordinate(10, 20, 30)
-    );
-    it('should return /spawnpoint @p[x=100,z=300] 10 20 30', function () {
-      assert.equal(spawn.toString(), '/spawnpoint @p[x=100,z=300] 10 20 30');
-    });
-  });
-
-});
-
-describe('Give', function () {
-
-  describe('#new(player, item, amount, data, dataTag)', function () {
-    let give = new Give(
-      new Player('Mike'),
-      new Item('apple'),
-      64,
-      1,
-      {display: {Lore: ["Apple here"]}}
-    );
-
-    it('should return /give Mike minecraft:apple 64 1 {display:{Lore:["Apple here"]}}', function () {
-      assert.equal(give.toString(), '/give Mike minecraft:apple 64 1 {display:{Lore:["Apple here"]}}');
-    });
-
-  });
-
-});
+}
