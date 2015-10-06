410095b1707f04c998962a2682d1a6482f4a08d7
Micooz <micooz@hotmail.com>
2015-10-06 12:32:02 +0800

    Added: Entity Names.

---
+++ src/constants/entities.js
@@ -0,0 +1,11 @@
+/**
+ * define all entities IDs
+ * http://minecraft.gamepedia.com/Data_values#Entity_IDs
+ *
+ * summon <EntityName> [x] [y] [z] [dataTag]
+ *
+ */
+let entities =
+  ["Item", "XPOrb", "LeashKnot", "Painting", "ItemFrame", "ArmorStand", "EnderCrystal", "ThrownEgg", "Arrow", "Snowball", "Fireball", "ThrownEnderpearl", "EyeOfEnderSignal", "ThrownPotion", "ThrownExpBottle", "WitherSkull", "FireworksRocketEntity", "PrimedTnt", "FallingSand", "MinecartCommandBlock", "Boat", "MinecartRideable", "MinecartChest", "MinecartFurnace", "MinecartTNT", "MinecartHopper", "MinecartSpawner", "Mob", "Monster", "Creeper", "Skeleton", "Spider", "Giant", "Zombie", "Slime", "Ghast", "PigZombie", "Enderman", "CaveSpider", "Silverfish", "Blaze", "LavaSlime", "EnderDragon", "WitherBoss", "Witch", "Endermite", "Guardian", "Shulker", "Rabbit", "Bat", "Pig", "Sheep", "Cow", "Chicken", "Squid", "Wolf", "MushroomCow", "SnowMan", "Ozelot", "VillagerGolem", "EntityHorse", "Rabbit", "Villager"];
+
+module.exports = entities;
