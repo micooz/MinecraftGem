0739fa1da06981172521714072ecf27e168a4107
Micooz <micooz@hotmail.com>
2015-10-11 20:00:41 +0800

    Added: tiletick dataTag.

---
+++ src/datatags/tiletick/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  TileTick: require('./tiletick')
+};
---
+++ src/datatags/tiletick/tiletick.js
@@ -0,0 +1,13 @@
+
+export default class TileTick {
+
+  constructor() {
+    this.i = String;
+    this.t = Number;
+    this.p = Number;
+    this.x = Number;
+    this.y = Number;
+    this.z = Number;
+  }
+
+}
